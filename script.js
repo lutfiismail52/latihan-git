ea75315f602f43f2a577b945df96f8c5f10e44ac
Lutfi Ismail <lutfiismail52@gmail.com>
2024-02-13 15:14:33 +0700

    two commit

--- script.js
+++
@@ -1,8 +0,0 @@
-// Fungsi untuk menghasilkan bilangan acak antara 1 dan 100
-function generateRandomNumber() {
-	return Math.floor(Math.random() * 100) + 1;
-}
-
-// Menggunakan fungsi untuk menghasilkan bilangan acak
-const randomNumber = generateRandomNumber();
-console.log("Bilangan acak antara 1 dan 100:", randomNumber);
